7bfac5bfd39024042ba927e7b6ee67e592a4064e
Zofia Nora <sophienora92@gmail.com>
2025-04-04 16:28:01 +0200

    added basic content and components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <>
+        <div className="container">
+            <h1>To Do List</h1>
+        </div>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+function Main() {
+    return (
+        <main>
+            <h1>Welcome to my website!</h1>
+            <p>This is the main content area.</p>
+        </main>
+    );
+}
+
+export default Main;
