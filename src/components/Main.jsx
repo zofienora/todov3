dbf27b4a8b11d35c6c513fa370e47acb1121445b
Zofia Nora <sophienora92@gmail.com>
2025-04-04 17:31:50 +0200

    added weekdays and input fields

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,129 @@
 function Main() {
+
+
+    
     return (
         <main>
+            <div className="container">
+                <div className="monday">
+                    <p>Monday</p>
+
+                    <div class="input-container">
+                        <input type="text" id="input" placeholder=" "/>
+                        <button id="addButton">➕</button>
+                    </div>
+
+                    <ul id="todo-list">
+                        <li>
+                            <div class="checkbox" id="checkbox"></div>
+                            <div class="listvalue" id="listvalue">
+                                <p id="inputfield"></p>
+                            </div>
+                            <div class="delete" id="delete"></div>
+                        </li>
+                    </ul>
+
+                </div>
+
+
+                <div className="tuesday">
+                    <p>Tuesday</p>
+
+                    <div class="input-container">
+                        <input type="text" id="input" placeholder=" "/>
+                        <button id="addButton">➕</button>
+                    </div>
+
+                    <ul id="todo-list">
+                        <li>
+                            <div class="checkbox" id="checkbox"></div>
+                            <div class="listvalue" id="listvalue">
+                                <p id="inputfield"></p>
+                            </div>
+                            <div class="delete" id="delete"></div>
+                        </li>
+                    </ul>
+                    
+                    
+                </div>
+
+                <div className="wednesday">
+                    <p>Wednesday</p>
+
+                    <div class="input-container">
+                        <input type="text" id="input" placeholder=" "/>
+                        <button id="addButton">➕</button>
+                    </div>
+
+                    <ul id="todo-list">
+                        <li>
+                            <div class="checkbox" id="checkbox"></div>
+                            <div class="listvalue" id="listvalue">
+                                <p id="inputfield"></p>
+                            </div>
+                            <div class="delete" id="delete"></div>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="thursday">
+                    <p>Thursday</p>
+
+                    <div class="input-container">
+                        <input type="text" id="input" placeholder=" "/>
+                        <button id="addButton">➕</button>
+                    </div>
+
+                    <ul id="todo-list">
+                        <li>
+                            <div class="checkbox" id="checkbox"></div>
+                            <div class="listvalue" id="listvalue">
+                                <p id="inputfield"></p>
+                            </div>
+                            <div class="delete" id="delete"></div>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="friday">
+                    <p>Friday</p>
+
+                    <div class="input-container">
+                        <input type="text" id="input" placeholder=" "/>
+                        <button id="addButton">➕</button>
+                    </div>
+
+                    <ul id="todo-list">
+                        <li>
+                            <div class="checkbox" id="checkbox"></div>
+                            <div class="listvalue" id="listvalue">
+                                <p id="inputfield"></p>
+                            </div>
+                            <div class="delete" id="delete"></div>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="sasu">
+                    <p>Saturday/Sunday</p>
+
+                    <div class="input-container">
+                        <input type="text" id="input" placeholder=" "/>
+                        <button id="addButton">➕</button>
+                    </div>
+
+                    <ul id="todo-list">
+                        <li>
+                            <div class="checkbox" id="checkbox"></div>
+                            <div class="listvalue" id="listvalue">
+                                <p id="inputfield"></p>
+                            </div>
+                            <div class="delete" id="delete"></div>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
             
         </main>
     );
